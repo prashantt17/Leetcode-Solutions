91afba6a976a5f3cd8ad89670ed76e0d38fede26
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-02-22 17:19:51 +0530

    Create 119. Pascal's Triangle II.java

---
+++ src/main/java/Arrays/119. Pascal's Triangle II.java
@@ -0,0 +1,35 @@
+/*
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+
+*/
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        int k = rowIndex;
+        Integer[] arr = new Integer[k + 1];
+        Arrays.fill(arr, 0);
+        arr[0] = 1;
+        
+        for (int i = 1; i <= k; i++) 
+            for (int j = i; j > 0; j--) 
+                arr[j] = arr[j] + arr[j - 1];
+        
+        return Arrays.asList(arr);
+    }
+}
