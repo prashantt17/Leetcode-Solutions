e78f04c6aac8230295edbb4ebbc8cbcafa044d04
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-02-22 17:13:53 +0530

    Create 74. Search a 2D Matrix.java

---
+++ src/main/java/Arrays/74. Search a 2D Matrix.java
@@ -0,0 +1,26 @@
+/*
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+*/
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0, j = matrix[0].length - 1;
+            while (i < matrix.length && j >= 0) {
+                    if (matrix[i][j] == target) {
+                        return true;
+                    } else if (matrix[i][j] > target) {
+                        j--;
+                    } else {
+                        i++;
+                    }
+                }
+             return false;
+    }
+}
