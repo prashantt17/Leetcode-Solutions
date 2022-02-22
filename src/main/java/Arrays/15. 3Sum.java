7a3c96cc2c130a2320ec5a237eb83463a9528240
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-02-22 16:56:47 +0530

    Create 15. 3Sum.java

---
+++ src/main/java/Arrays/15. 3Sum.java
@@ -0,0 +1,40 @@
+/*
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+*/
+
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+         List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    for (int i = 0; i + 2 < nums.length; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) {              
+            continue;
+        }
+        int j = i + 1, k = nums.length - 1;  
+        int target = -nums[i];
+        while (j < k) {
+            if (nums[j] + nums[k] == target) {
+                res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1]) j++;  
+                while (j < k && nums[k] == nums[k + 1]) k--;  
+            } else if (nums[j] + nums[k] > target) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return res;
+    }
+}
