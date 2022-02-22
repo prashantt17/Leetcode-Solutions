a040b6f92f0e98c46b7e309ceb85dc4c5c131317
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-02-22 17:35:33 +0530

    Create 376. Wiggle Subsequence.java

---
+++ src/main/java/Arrays/376. Wiggle Subsequence.java
@@ -0,0 +1,57 @@
+/*
+
+A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with one element and a sequence with two non-equal elements are trivially wiggle sequences.
+
+For example, [1, 7, 4, 9, 2, 5] is a wiggle sequence because the differences (6, -3, 5, -7, 3) alternate between positive and negative.
+In contrast, [1, 4, 7, 2, 5] and [1, 7, 4, 5, 5] are not wiggle sequences. The first is not because its first two differences are positive, and the second is not because its last difference is zero.
+A subsequence is obtained by deleting some elements (possibly zero) from the original sequence, leaving the remaining elements in their original order.
+
+Given an integer array nums, return the length of the longest wiggle subsequence of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,7,4,9,2,5]
+Output: 6
+Explanation: The entire sequence is a wiggle sequence with differences (6, -3, 5, -7, 3).
+Example 2:
+
+Input: nums = [1,17,5,10,13,15,10,5,16,8]
+Output: 7
+Explanation: There are several subsequences that achieve this length.
+One is [1, 17, 10, 13, 10, 16, 8] with differences (16, -7, 3, -3, 6, -8).
+Example 3:
+
+Input: nums = [1,2,3,4,5,6,7,8,9]
+Output: 2
+
+*/
+
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        if( nums.length == 0 ) return 0;
+        
+        int[] up = new int[nums.length];
+        int[] down = new int[nums.length];
+        
+        up[0] = 1;
+        down[0] = 1;
+        
+        for(int i = 1 ; i < nums.length; i++){
+            if( nums[i] > nums[i-1] ){
+                up[i] = down[i-1]+1;
+                down[i] = down[i-1];
+            }else if( nums[i] < nums[i-1]){
+                down[i] = up[i-1]+1;
+                up[i] = up[i-1];
+            }else{
+                down[i] = down[i-1];
+                up[i] = up[i-1];
+            }
+        }
+        
+        return Math.max(down[nums.length-1],up[nums.length-1]);
+    }
+}
+
