b04b62e25585378f1504d1d10e799e3172d21631
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-02-22 17:34:00 +0530

    Create 334. Increasing Triplet Subsequence.java

---
+++ src/main/java/Arrays/334. Increasing Triplet Subsequence.java
@@ -0,0 +1,35 @@
+/*
+
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+Example 3:
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+*/
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+         int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= small) { small = n; } // update small if n is smaller than both
+            else if (n <= big) { big = n; } // update big only if greater than small but smaller than big
+            else return true; // return if you find a number bigger than both
+        }
+        return false;
+    }
+}
