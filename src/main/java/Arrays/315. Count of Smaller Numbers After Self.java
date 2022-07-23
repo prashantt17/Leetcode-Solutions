40a66a7073075ca0e9089fa93715b955a7983e43
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-07-23 18:55:27 +0530

    Create 315. Count of Smaller Numbers After Self.java

---
+++ src/main/java/Arrays/315. Count of Smaller Numbers After Self.java
@@ -0,0 +1,59 @@
+/*
+
+You are given an integer array nums and you have to return a new counts array. The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,6,1]
+Output: [2,1,1,0]
+Explanation:
+To the right of 5 there are 2 smaller elements (2 and 1).
+To the right of 2 there is only 1 smaller element (1).
+To the right of 6 there is 1 smaller element (1).
+To the right of 1 there is 0 smaller element.
+Example 2:
+
+Input: nums = [-1]
+Output: [0]
+Example 3:
+
+Input: nums = [-1,-1]
+Output: [0,0]
+
+*/
+
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        
+        int min = 20001;
+        int max = -1;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        
+        min--;
+        int[] count = new int[max-min+1];
+        Integer[] result = new Integer[nums.length];
+        for (int i = nums.length-1; i >=0; i--) {
+            int k = nums[i]-min-1;
+            int c = 0;
+            do {
+                c += count[k];
+                k -= (-k&k);
+            } while (k > 0);
+            result[i] = c;
+            
+            k = nums[i]-min;
+            while (k < count.length) {
+                count[k]++;
+                k += (-k&k);
+            }
+        }
+        
+        return Arrays.asList(result);
+        
+    }
+}
