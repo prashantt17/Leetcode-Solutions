9ed5e9ebc7b16bc22a3bf3a11594e9f0b955a264
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-08-04 00:57:24 +0530

    Create 300. Longest Increasing Subsequence.java

---
+++ src/main/java/DynamicProgramming/300. Longest Increasing Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+    int size = 0;
+    for (int x : nums) {
+        int i = 0, j = size;
+        while (i != j) {
+            int m = (i + j) / 2;
+            if (tails[m] < x)
+                i = m + 1;
+            else
+                j = m;
+        }
+        tails[i] = x;
+        if (i == size) ++size;
+    }
+    return size;
+    }
+}
