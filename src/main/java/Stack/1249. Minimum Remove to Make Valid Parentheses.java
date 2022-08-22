ae057eb1f84b054772b8daa8f4380c92a4b4859a
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-08-23 01:38:00 +0530

    Create 1249. Minimum Remove to Make Valid Parentheses.java

---
+++ src/main/java/Stack/1249. Minimum Remove to Make Valid Parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+  Stack<Integer> st = new Stack<>();
+  for (int i = 0; i < sb.length(); ++i) {
+    if (sb.charAt(i) == '(') st.add(i);
+    if (sb.charAt(i) == ')') {
+      if (!st.empty()) st.pop();
+      else sb.setCharAt(i, '*');
+    }
+  }
+  while (!st.empty())
+    sb.setCharAt(st.pop(), '*');
+  return sb.toString().replaceAll("\\*", ""); 
+    }
+}
