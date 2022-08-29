bcd211e8d09c1e9fa61cb333fd0fded16e247804
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-08-29 15:57:01 +0530

    Create 1539. Kth Missing Positive Number.java

---
+++ src/main/java/Arrays/1539. Kth Missing Positive Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end-start) / 2; 
+            if(arr[mid]-(mid+1)<k){
+                start = mid +1;
+            } else{
+                end = mid -1;
+            }
+        }
+        return start + k; 
+    }
+}
