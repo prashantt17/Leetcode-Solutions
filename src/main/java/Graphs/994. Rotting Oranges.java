bb14663e55834e1544ab08f78136979102b54894
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-09-03 00:18:10 +0530

    Create 994. Rotting Oranges.java

---
+++ src/main/java/Graphs/994. Rotting Oranges.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if(grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int count_fresh = 0;
+        for(int i = 0 ; i < rows ; i++) {
+            for(int j = 0 ; j < cols ; j++) {
+                if(grid[i][j] == 2) {
+                    queue.offer(new int[]{i , j});
+                }
+                else if(grid[i][j] == 1) {
+                    count_fresh++;
+                }
+            }
+        }
+        if(count_fresh == 0) return 0;
+        int count = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!queue.isEmpty()) {
+            ++count;
+            int size = queue.size();
+            for(int i = 0 ; i < size ; i++) {
+                int[] point = queue.poll();
+                for(int dir[] : dirs) {
+                    int x = point[0] + dir[0];
+                    int y = point[1] + dir[1];
+                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] == 2) continue;
+                    grid[x][y] = 2;
+                    queue.offer(new int[]{x , y});
+                    count_fresh--;
+                }
+            }
+        }
+        return count_fresh == 0 ? count-1 : -1;
+    }
+}
