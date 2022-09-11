33a530b81f42f9160b413c9046c43df45f3415a5
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-09-12 01:15:51 +0530

    Create 1383. Maximum Performance of a Team.java

---
+++ src/main/java/Greedy/1383. Maximum Performance of a Team.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        int[][] players = new int[n][2];
+        for (int i=0; i<n; i++) {
+            players[i][0] = efficiency[i];
+            players[i][1] = speed[i];
+        }
+		Arrays.sort(players, (p1, p2) -> (p2[0] - p1[0]));
+
+		PriorityQueue<Integer> speedQueue = new PriorityQueue<>(k);
+        long teamPerformance = 0, teamSpeed = 0;
+
+        for (int i=0; i<n; i++) {
+		    if (speedQueue.size() >= k) {
+                teamSpeed -= speedQueue.remove();
+            }
+            speedQueue.add(players[i][1]);
+            teamSpeed += players[i][1];
+
+            teamPerformance = Math.max(teamPerformance, teamSpeed * players[i][0]);
+        }
+        return (int) (teamPerformance % 1000000007);
+    }
+}
