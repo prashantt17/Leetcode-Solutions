287aa0fd0f6b7255e1ebc7dfaff08fd0cb38e1ba
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-09-13 17:19:13 +0530

    Create 611. Valid Triangle Number.java

---
+++ src/main/java/Arrays/611. Valid Triangle Number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int ans=0;
+        if(nums.length<3) 
+            return ans;
+
+        Arrays.sort(nums);
+
+        for(int i=2;i<nums.length;i++)
+        {
+            int left=0,right=i-1;
+            while(left<right)
+           {
+            if(nums[left]+nums[right]>nums[i])
+            {
+                ans= ans+(right-left);
+                right--;
+            }
+            else
+            left++;
+          }
+        }
+        return ans;
+    }
+}
