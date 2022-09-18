5514486b488dafa84404beddab6505c55f7943a1
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-09-18 19:37:48 +0530

    Create 287. Find the Duplicate Number.java

---
+++ src/main/java/Math/287. Find the Duplicate Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+         int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+}
