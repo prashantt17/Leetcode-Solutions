babc54c16911397a6b931a3149416d33c38e1317
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-09-27 01:31:32 +0530

    Update 335. Self Crossing.java

--- src/main/java/Arrays/335. Self Crossing.java
+++ src/main/java/Arrays/335. Self Crossing.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean isSelfCrossing(int[] distance) {
         boolean arm = false;
         boolean leg = false;
-        for (int i = 2; i < x.length; ++i) {
-        int a = f(x, i - 2) - f(x, i - 4);
-        int b = f(x, i - 2);
+        for (int i = 2; i < distance.length; ++i) {
+        int a = f(distance, i - 2) - f(distance, i - 4);
+        int b = f(distance, i - 2);
 
-        if (arm && x[i] >= b)          return true;  
-        if (leg && x[i] >= a && a > 0) return true;  
+        if (arm && distance[i] >= b)          return true;  
+        if (leg && distance[i] >= a && a > 0) return true;  
 
-        if (x[i] < a)       arm = true;
-        else if (x[i] <= b) leg = true;
+        if (distance[i] < a)       arm = true;
+        else if (distance[i] <= b) leg = true;
         }
         return false;
         }
-        private int f(int[] x, int index) {
-        return (index < 0) ? 0 : x[index];
+        private int f(int[] distance, int index) {
+        return (index < 0) ? 0 : distance[index];
     }
 }
