f219787c215b2937f9da1a210aac370438984a62
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-09-28 13:44:16 +0530

    Create 1300. Sum of Mutated Array Closest to Target.java

---
+++ src/main/java/Arrays/1300. Sum of Mutated Array Closest to Target.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findBestValue(int[] arr, int target) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for (int a : arr) {
+            sum += a;
+            max = Math.max(max, a);
+        }
+        if (sum == target) return max;
+        int min = 0, res = 1,diff = Integer.MAX_VALUE;
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+            sum = getMutatedSum(arr, mid); 
+            if (sum > target) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+            
+            if (Math.abs(sum - target) < diff || (Math.abs(sum - target) == diff && mid < res)) { 
+                res = mid;
+                diff = Math.abs(sum - target);
+            }
+        }
+        return res;
+    }
+
+    private int getMutatedSum(int[] arr, int mid) {
+        int sum = 0;
+        for (int a : arr) {
+            sum += (a > mid) ? mid : a;
+        }
+        return sum;
+    }
+}
