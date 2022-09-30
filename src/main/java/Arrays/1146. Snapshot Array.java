145df35b2e343ee1fd383c0cd9364eabfb90801a
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-09-30 16:35:00 +0530

    Create 1146. Snapshot Array.java

---
+++ src/main/java/Arrays/1146. Snapshot Array.java
@@ -0,0 +1,32 @@
+class SnapshotArray {
+
+    TreeMap<Integer, Integer>[] A;
+    int snap_id = 0;
+    public SnapshotArray(int length) {
+        A = new TreeMap[length];
+        for (int i = 0; i < length; i++) {
+            A[i] = new TreeMap<Integer, Integer>();
+            A[i].put(0, 0);
+        }
+    }
+
+    public void set(int index, int val) {
+        A[index].put(snap_id, val);
+    }
+
+    public int snap() {
+        return snap_id++;
+    }
+
+    public int get(int index, int snap_id) {
+        return A[index].floorEntry(snap_id).getValue();
+    }
+}
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length);
+ * obj.set(index,val);
+ * int param_2 = obj.snap();
+ * int param_3 = obj.get(index,snap_id);
+ */
