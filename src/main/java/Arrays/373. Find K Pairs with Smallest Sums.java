45212ce965f5712b131fefe76194c336cce68835
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-10-01 14:25:07 +0530

    Create 373. Find K Pairs with Smallest Sums.java

---
+++ src/main/java/Arrays/373. Find K Pairs with Smallest Sums.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> (a[0] + a[1]) - (b[0] + b[1]));
+        
+        for(int i=0; i < nums1.length && i < k; i++)
+            minHeap.add(new int[]{nums1[i], nums2[0], 0});
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i=0; i < k && !minHeap.isEmpty(); i++){
+            int[] curr = minHeap.poll();
+            result.add(List.of(curr[0], curr[1]));
+            int nums2Idx = curr[2];
+            if(nums2Idx < nums2.length - 1)
+                minHeap.add(new int[]{curr[0], nums2[nums2Idx + 1], nums2Idx + 1});
+        }
+        return result;
+    }
+}
