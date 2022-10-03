53eddb715d2a0212d4c418683d003234fe97943c
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-10-03 15:25:08 +0530

    Create 395. Longest Substring with At Least K Repeating Characters.java

---
+++ src/main/java/Strings/395. Longest Substring with At Least K Repeating Characters.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        char[] str = s.toCharArray();
+        int[] counts = new int[26];
+        int h, i, j, idx, max = 0, unique, noLessThanK;
+        
+        for (h = 1; h <= 26; h++) {
+            Arrays.fill(counts, 0);
+            i = 0; 
+            j = 0;
+            unique = 0;
+            noLessThanK = 0;
+            while (j < str.length) {
+                if (unique <= h) {
+                    idx = str[j] - 'a';
+                    if (counts[idx] == 0)
+                        unique++;
+                    counts[idx]++;
+                    if (counts[idx] == k)
+                        noLessThanK++;
+                    j++;
+                }
+                else {
+                    idx = str[i] - 'a';
+                    if (counts[idx] == k)
+                        noLessThanK--;
+                    counts[idx]--;
+                    if (counts[idx] == 0)
+                        unique--;
+                    i++;
+                }
+                if (unique == h && unique == noLessThanK)
+                    max = Math.max(j - i, max);
+            }
+        }
+        
+        return max;
+    }
+}
