b154ba513b9f92ae37fa14feb84c9c04dcc39b2d
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-10-09 18:04:36 +0530

    Create 743. Network Delay Time.java

---
+++ src/main/java/Graphs/743. Network Delay Time.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, Map<Integer,Integer>> map = new HashMap<>();
+        for(int[] time : times){
+            map.putIfAbsent(time[0], new HashMap<>());
+            map.get(time[0]).put(time[1], time[2]);
+        }
+        
+        Queue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0] - b[0]));
+        
+        pq.add(new int[]{0, k});
+        
+        boolean[] visited = new boolean[n+1];
+        int res = 0;
+        
+        while(!pq.isEmpty()){
+            int[] cur = pq.remove();
+            int curNode = cur[1];
+            int curDist = cur[0];
+            if(visited[curNode]) continue;
+            visited[curNode] = true;
+            res = curDist;
+            n--;
+            if(map.containsKey(curNode)){
+                for(int next : map.get(curNode).keySet()){
+                    pq.add(new int[]{curDist + map.get(curNode).get(next), next});
+                }
+            }
+        }
+        return n == 0 ? res : -1;
+    }
+}
