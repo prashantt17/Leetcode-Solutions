3e0094f94ba75bdf54194d0f787185382e5b4ce1
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-10-10 15:14:34 +0530

    Create 739. Daily Temperatures.java

---
+++ src/main/java/Arrays/739. Daily Temperatures.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(top > -1 && temperatures[i] > temperatures[stack[top]]) {
+                int idx = stack[top--];
+                ret[idx] = i - idx;
+            }
+            stack[++top] = i;
+        }
+        return ret;
+    }
+}
