24005daf7b28678750dea41ba9ca6192336404ec
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-11-11 17:03:57 +0530

    Create 636. Exclusive Time of Functions.java

---
+++ src/main/java/Stack/636. Exclusive Time of Functions.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+         int[] res = new int[n];
+    Stack<Integer> stack = new Stack<>();
+    int prevTime = 0;
+    for (String log : logs) {
+        String[] parts = log.split(":");
+        if (!stack.isEmpty()) res[stack.peek()] +=  Integer.parseInt(parts[2]) - prevTime; 
+        prevTime = Integer.parseInt(parts[2]);
+        if (parts[1].equals("start")) stack.push(Integer.parseInt(parts[0]));
+        else {
+            res[stack.pop()]++;
+            prevTime++;
+        }
+    }
+    return res;
+    }
+}
