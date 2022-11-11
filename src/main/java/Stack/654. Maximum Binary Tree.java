11782052e2acea494b736967ee917f55c789b2a8
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-11-11 17:04:31 +0530

    Create 654. Maximum Binary Tree.java

---
+++ src/main/java/Stack/654. Maximum Binary Tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+         Deque<TreeNode> stack = new LinkedList<>();
+        for(int i = 0; i < nums.length; i++) {
+            TreeNode curr = new TreeNode(nums[i]);
+            while(!stack.isEmpty() && stack.peek().val < nums[i]) {
+                curr.left = stack.pop();
+            }
+            if(!stack.isEmpty()) {
+                stack.peek().right = curr;
+            }
+            stack.push(curr);
+        }
+        
+        return stack.isEmpty() ? null : stack.removeLast();
+    }
+}
