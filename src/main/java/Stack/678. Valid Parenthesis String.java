d28497901632808c4aa66e78b5beeb70578b84e0
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-11-11 17:05:12 +0530

    Create 678. Valid Parenthesis String.java

---
+++ src/main/java/Stack/678. Valid Parenthesis String.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkValidString(String s) {
+         Stack<Integer> leftID = new Stack<>();
+        Stack<Integer> starID = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(')
+                leftID.push(i);
+            else if (ch == '*')
+                starID.push(i);
+            else {
+                if (leftID.isEmpty() && starID.isEmpty())   return false;
+                if (!leftID.isEmpty())
+                    leftID.pop();
+                else 
+                    starID.pop();
+            }
+        }
+        while (!leftID.isEmpty() && !starID.isEmpty()) {
+            if (leftID.pop() > starID.pop()) 
+                return false;
+        }
+        return leftID.isEmpty();
+    }
+}
