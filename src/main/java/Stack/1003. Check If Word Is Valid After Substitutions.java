20abab45611b68e98c1cdb79520713ffdc10b86f
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-11-13 02:13:58 +0530

    Create 1003. Check If Word Is Valid After Substitutions.java

---
+++ src/main/java/Stack/1003. Check If Word Is Valid After Substitutions.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c == 'c') {
+                if (stack.isEmpty() || stack.pop() != 'b') return false;
+                if (stack.isEmpty() || stack.pop() != 'a') return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
