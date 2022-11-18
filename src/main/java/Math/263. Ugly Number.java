ef11736d7f828070d44d0aa414fa13b6f4d78547
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-11-19 01:56:01 +0530

    Create 263. Ugly Number.java

---
+++ src/main/java/Math/263. Ugly Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n>0 && n%5==0){
+            n=n/5;
+        }
+        while(n>0 && n%3==0){
+            n=n/3;
+        }
+        while(n>0 && n%2==0){
+            n=n/2;
+        }
+        return n==1;
+    }
+}
